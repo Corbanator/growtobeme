57f4f6b3cf360ce8462b631912e2faf0202ef3a9
Thomas <thomas.ed.dick@gmail.com>
2022-09-12 20:15:24 -0600

    SCORE FREIND WEORKING!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!

--- app/Controllers/Freinds.php
+++ app/Controllers/Freinds.php
@@ -23,21 +23,27 @@ class Freinds extends BaseController
 
         $scores = array();
 
-        // foreach($friendArray as $friend){
-        // //     $score = [
-        // //         "username" => $userModel->select("username")->where("id", $friend['friend2'])->get()->getResult(),
-        // //         "game" => $scoreModel->select("gameid")->where("userid", $friend['friend2'])->get()->getResult(),
-        // //         "score" => $scoreModel->select("score")->where("userid", $friend['friend2'])->get()->getResult()
-        // //     ];
+        for($i = 0; $i < count($friendArray); $i++){
+            $result = $scoreModel->getScore($friendArray[$i]["friend2"]);
 
-        // //     array_push($scores, $score);
-        // }
+            $result = json_decode(json_encode($result), true);
 
-        $stuff = $scoreModel->getScore(1);
+            $username = $result[0]["username"];
+            $gameName = $result[0]["gameName"];
+            $score = $result[0]["score"];
+
+            $score = [
+                "username" => $username,
+                "game" => $gameName,
+                "score" => $score
+            ];
+
+            array_push($scores, $score);
+        }
 
         $data = [
             "site_title" => "Friends",
-            "scores" => json_encode($stuff)
+            "scores" => json_encode($scores)
         ];
         
         return view("friends", $data);
